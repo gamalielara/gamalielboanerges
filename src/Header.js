a72f1d964c8d95ab833dbb840e7152edcd39be58
gamalielara <ara.gamaliel@ui.ac.id>
2021-10-26 05:53:18 +0700

    fixed navbar bugs and added animations

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
 
 const Header = () => {
     const [clicked, setClicked] = useState(false); 
@@ -20,11 +21,11 @@ const Header = () => {
             <nav className="small-nav-menu fixed top-0 right-0 w-full sm:hidden">
                 <div className={clicked ? "full-menu w-screen h-screen bg-black p-4 relative" : "hidden"}>
                     <button className="menu-bars absolute top-4 right-4" onClick={() => setClicked(false)}><i class="fas fa-times text-3xl text-white"></i></button>
-                    <div className="menus w-full h-full flex flex-col justify-center items-center">
-                        <button className="hover:bg-gum-red rounded py-1 px-2 transition-all" onClick={() => setClicked(false)}><h1 className="text-lg text-white"><Link to="/">Home</Link></h1></button>
-                        <button className="hover:bg-gum-red rounded py-1 px-2 transition-all" onClick={() => setClicked(false)}><h1 className="text-lg text-white"><Link to='/projects'>My Skills & Works</Link></h1></button>
-                        <button className="hover:bg-gum-red rounded py-1 px-2 transition-all" onClick={() => setClicked(false)}><h1 className="text-lg text-white"><a href="https://www.gumrindelwald.com" target="_blank">My Blog</a></h1></button>
-                        <button className="hover:bg-gum-red rounded py-1 px-2 transition-all" onClick={() => setClicked(false)}><h1 className="text-lg text-white"><Link to="/about-me">About Me</Link></h1></button>
+                    <div className="menus w-full h-full flex flex-col justify-center items-center gap-8">
+                        <Link to="/"><button className="hover:bg-gum-red rounded py-1 px-2 transition-all" onClick={() => setClicked(false)}><motion.h1 className="text-lg text-white" initial={clicked ? {y:"0%"} : {}} animate={clicked ? {y:"-100%"} : {}} transition={{duration: .25}}>Home</motion.h1></button></Link>
+                        <Link to='/projects'><button className="hover:bg-gum-red rounded py-1 px-2 transition-all" onClick={() => setClicked(false)}><motion.h1 className="text-lg text-white" initial={clicked ? {y:"0%"} : {}} animate={clicked ? {y:"-100%"} : {}} transition={{duration: .25, delay: .25}}>My Skills & Works</motion.h1></button></Link>
+                        <Link to='/gumrindelwald'><button className="hover:bg-gum-red rounded py-1 px-2 transition-all" onClick={() => setClicked(false)}><motion.h1 className="text-lg text-white" initial={clicked ? {y:"0%"} : {}} animate={clicked ? {y:"-100%"} : {}} transition={{duration: .25, delay: .5}}>My Blog</motion.h1></button></Link>
+                        <Link to="/about-me"><button className="hover:bg-gum-red rounded py-1 px-2 transition-all" onClick={() => setClicked(false)}><motion.h1 className="text-lg text-white" initial={clicked ? {y:"0%"} : {}} animate={clicked ? {y:"-100%"} : {}} transition={{duration: .25, delay: .75}}>About Me</motion.h1></button></Link>
                     </div>
                 </div>
                 <div className="nav-container w-full relative">
